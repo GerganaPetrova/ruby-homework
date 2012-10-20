2191e2dc849b3ff26df05b5b6068d14e6b206a31
Gergana Petrova <gergana.petrova.petrova@gmail.com>
2012-10-20 16:41:46 +0300

    Add to sample-spec

--- 01/sample_spec.rb
+++ 01/sample_spec.rb
@@ -14,10 +14,27 @@ describe "Hash#group_values" do
   it "maps each value to an array of keys" do
     {a: 1}.group_values.should eq 1 => [:a]
   end
+
+  it "works with no recurring values" do
+    h = {a: 1, b: 2, c: 3, d: 8}
+    h.group_values.should eq 1 => [:a], 2 => [:b], 3 => [:c], 8 => [:d]
+  end
+
+  it "does something useful" do
+    {a: 1, b: 3, c: 2, d: 2, e: 1}.group_values.should eq 1 => [:a, :e], 3 => [:b], 2 => [:c, :d]
+  end
 end
 
 describe "Array#densities" do
   it "maps each element to the number of occurences in the original array" do
     [:a, :b, :a].densities.should eq [2, 1, 2]
+  end
+
+  it "when the array is empty" do
+    [].densities.should eq []
+  end
+
+  it "when the array is inhomogeneous" do
+    [:a, 1, 'a', 1, 'a'].densities.should eq [1,2,2,2,2]
   end
 end
